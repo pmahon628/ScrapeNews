8c6f66f101bb92d62a2c99a42e802ae359fa8550
Patrick Mahon <patrickmahon@Patricks-MacBook-Pro.local>
2019-08-02 19:21:20 -0400

    work

--- Public/app.js
+++ Public/app.js
@@ -1,6 +1,6 @@
 $.getJSON('/articles', function(Data){
     for (var i =0; i < Data.length; i++){
-        $("articles").append("<p data-id" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+        $("news").append("<p data-id" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
       }
     });
 
@@ -18,7 +18,7 @@ $(document).on('click', 'p', function(){
         $('#notes').append('<h2>' + data.title + '</h2>');
         $('#notes').append("<input id='titleinput' name='title' >");
         $('#notes').append("<textarea id ='bodyinput name='body'></textarea");
-        $('#notes').appen("<button data-id='" + data._id + "'id=savenote'>Save Note</button>");
+        $('#notes').append("<button data-id='" + data._id + "'id=savenote'>Save Note</button>");
 
         if(data.note){
             $('#titleinput').val(data.note.val);
@@ -32,12 +32,12 @@ $(document).on('click', '#savenote', function(){
 
 
 $.ajax({
-    method: "POST";
+    method: "POST",
     url: '/articles/' +  thisId,
     data: {
-        title: $('#titleinput').val();
-        body: $('#bodyinput').val();  
-    }
+        title: $('#titleinput').val(),
+        body: $('#bodyinput').val()
+    },
 })
     .then(function(data){
         console.log(data);
